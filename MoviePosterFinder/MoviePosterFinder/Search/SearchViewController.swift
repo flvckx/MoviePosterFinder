bda9940ce095c884746be7cb33e94b4241d4682e
Serhii <sergiy.palash@ekreative.com>
2018-02-18 13:17:53 +0200

    search ui added

---
+++ MoviePosterFinder/MoviePosterFinder/Search/SearchViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MoviePosterFinder
+//
+//  Created by Serhii Palash on 18/02/2018.
+//  Copyright © 2018 Serhii Palash. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private weak var movieNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func searchTouched(_ sender: Any) {
+        movieNameTextField.endEditing(true)
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+    }
+}
+
